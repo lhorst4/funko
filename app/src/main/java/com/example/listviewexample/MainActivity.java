3d784e307edbd88fa01998d65c9c174ca6b22fec
Levi Horst <levi.horst@bruins.belmont.edu>
2023-09-26 13:46:05 -0500

    added on item click listener

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -3,17 +3,25 @@ package com.example.listviewexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.LinkedList;
 
 import model.Pet;
 
 public class MainActivity extends AppCompatActivity {
-
     ListView list;
-
+    AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener(){
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+            Toast.makeText(getApplicationContext(), list.getSelectedItemPosition(), Toast.LENGTH_LONG).show();
+        }
+    };
 
 
     @Override
@@ -21,14 +29,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         list = findViewById(R.id.petLV);
+        list.setOnItemClickListener(listener);
 
-        int selection = 1;
+        int selection = 2;
         populateListView(selection);
-
     }
-
     private void populateListView(int selection) {
         switch (selection) {
             case 1:
@@ -37,10 +43,8 @@ public class MainActivity extends AppCompatActivity {
             case 2:
                 usingDataArray();
                 break;
-
         }
     }
-
     /**
      * This method populates the list view using a string resource containing
      * the content of a strings array resource
@@ -49,9 +53,7 @@ public class MainActivity extends AppCompatActivity {
         String[] values = getResources().getStringArray(R.array.breeds);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
         list.setAdapter(adapter);
-
     }
-
     /**
      * This method populates the list view using a string resource containing the list of a data array
      */
@@ -62,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
         list2.add(new Pet("jeff", "Lab", 3));
         list2.add(new Pet("bartholemeu", "Yorki", 15));
         list2.add(new Pet("forest", "Chihuahua", 2));
-
         list.setAdapter(adapter);
     }
 }
